dbc92e97606aa1e935652c3a853151588d39f20b
kissa1001 <kissa101295@gmail.com>
2016-11-19 12:18:56 -0800

    project initialization

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+
+export default function () {
+    return [
+        {title: 'Javascript: The Good Parts'},
+        {title: 'Harry Potter: part 1'},
+        {title: 'Harry Potter: part 2'},
+        {title: 'Harry Potter: part 3'},
+        {title: 'The Dark Tower'},
+        {title: 'Princess Mononoco'},
+        {title: 'Hamlet'}
+    ]
+}
